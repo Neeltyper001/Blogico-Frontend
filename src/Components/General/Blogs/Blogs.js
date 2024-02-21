188f8296a4d5ce32e0006b2072ac221e937fcbd9
Neelesh Joshi <neeleshjoshi001@gmail.com>
2024-02-21 18:13:05 +0530

    # search by username parameters

--- src/Components/General/Blogs/Blogs.js
+++ src/Components/General/Blogs/Blogs.js
@@ -1,22 +1,23 @@
 import React from 'react'
 import './index.css'
-import Db from '../../../db.json'
 import Blog from '../Blog/Blog.js'
 import { useState,useEffect } from 'react'
 import axios from 'axios'
-
+import { useLocation } from 'react-router-dom'
 const Blogs = () => {
   const [blogs, setBlogs] = useState([]);
-
+  const {search} = useLocation();
+  console.log(search);
+  console.log(typeof(search))
   useEffect(()=>{
 
     const getBlogs = async ()=>{
-      const res = await axios.get('http://localhost:5000/api/posts')
+      const res = await axios.get('http://localhost:5000/api/posts'+search)
       console.log(res.data)
       setBlogs(res.data);
     }
     getBlogs();
-  },[])
+  },[search])
   const blogsArr = blogs.map((blog,index)=>{
     return (
       <Blog
